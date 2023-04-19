078dcf8d211b5f18a96c92a2cbaa8539b3b18b07
Linhchi162 <linhchi1622k4@gmail.com>
2023-04-20 01:37:47 +0700

    mk

--- PacCat!/Map.cpp
+++ PacCat!/Map.cpp
@@ -1,35 +1,35 @@
 #include "Map.h"
-#include "Game.h"
-#include<fstream>
 
 
 
 Map::Map()
 {
+	tWall = NULL;
+	tFood = NULL;
+	mWidth = 0;
+	mHeight = 0;
+	foodNumber = 0;
 
+	lastAllowcateFoodTime = 0;
+	specialFoodType = 0;
+	hasSpecialFood = false;
+	allowcateSpecialFoodCount = 0;
+	specialFoodTime = 15000;
+	lastSpecialFoodTime = 0;
+
+	specialFoodX = 10;
+	specialFoodY = 15;
+
+	specialFoodPoint.clear();
+	specialFoodPoint.push_back(100);
+	specialFoodPoint.push_back(300);
+	specialFoodPoint.push_back(500);
+	specialFoodPoint.push_back(700);
+	specialFoodPoint.push_back(1000);
+	specialFoodPoint.push_back(2000);
+	specialFoodPoint.push_back(3000);
+	specialFoodPoint.push_back(5000);
+
+	map.clear();
+	cornerMap.clear();
 }
-Map::~Map()
-{
-
-}
-
-void Map::LoadMap(std::string path, int sizeX, int sizeY)
-{
-	char tile;
-	std::fstream mapFile;
-	mapFile.open(path);
-
-	for (int y = 0; y < sizeY; y++)
-	{
-		for (int x = 0; x < sizeX; x++)
-		{
-			mapFile.get(tile);
-			Game::AddTile(atoi(&tile), x * 32, y * 32);
-			mapFile.ignore();
-
-		}
-	}
-
-	mapFile.close();
-}
-
