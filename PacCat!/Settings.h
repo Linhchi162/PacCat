99307f413a67fb1314eb5df94001af8ac05f2f7d
Linhchi162 <linhchi1622k4@gmail.com>
2023-05-02 22:45:41 +0700

    settings

--- PacCat!/Settings.h
+++ PacCat!/Settings.h
@@ -1,23 +1,20 @@
 #pragma once
-#ifndef _SETTINGS_HEADER_INCLUDED_
-#define _SETTINGS_HEADER_INCLUDED_
- // negative => the number of dots on map. Change this value to a positive int to 
-// explicitly set the amount of score to be required for winning.
 
-const unsigned COLLIDER_BOX_SIZE = 20; // these collider boxes are used for Pacman/Ghosts colliding logic
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
 
-const int tilesCountX = 19, tilesCountY = 19;
-const unsigned TILE_SIZE = 32;// Textures are 32x32p. Having higher tile size will decrease quality and might affect speeds.
-const unsigned HALF_TILE_SIZE = TILE_SIZE / 2;
-const unsigned WINDOW_WIDTH = tilesCountX * TILE_SIZE;
-const unsigned WINDOW_HEIGHT = tilesCountY * TILE_SIZE;
-const unsigned PACMAN_START_XPOS = 9 *TILE_SIZE;
-const unsigned PACMAN_START_YPOS = 12 * TILE_SIZE;
+using namespace std;
 
+struct Vec2 {
+	int x;
+	int y;
+};
 
-const int pacmanSpeed = 2;
+const int TILE_SIZE = 64;
+const int TILE_ROWS = 10;
+const int TILE_COLS = 10;
+const int SCREEN_WIDTH = TILE_SIZE * TILE_COLS;
+const int SCREEN_HEIGHT = TILE_SIZE * TILE_ROWS;
 
-
-
-#endif
 
