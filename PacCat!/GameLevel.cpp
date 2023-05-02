b32f3013877c193b4bd69b1ba5867db987abda0f
Linhchi162 <linhchi1622k4@gmail.com>
2023-05-02 22:59:28 +0700

    level

---
+++ PacCat!/GameLevel.cpp
@@ -0,0 +1,29 @@
+#include "GameLevel.h"
+
+void GameLevel::LoadLevel() {
+	int rowCount = 0;
+	int colCount = 0;
+
+	string path = "Levels/level" + to_string(currentLevel) + ".txt";
+
+	levelFile.open(path);
+	if (levelFile.is_open()) {
+		char inChar;
+		while (levelFile.get(inChar)) {
+			if (inChar == '\n') {
+				colCount = 0;
+				rowCount++;
+			}
+			else {
+				levelMap[colCount][rowCount] = inChar;
+				colCount++;
+			}
+		}
+	}
+
+	levelFile.close();
+}
+
+void GameLevel::UpdateLevel() {
+	currentLevel++;
+}
---
+++ PacCat!/GameLevel.h
@@ -0,0 +1,18 @@
+#pragma once
+#pragma once
+
+#include "Settings.h"
+#include <fstream>
+#include <string>
+
+class GameLevel
+{
+public:
+	void LoadLevel();
+	void UpdateLevel();
+	char levelMap[TILE_ROWS][TILE_COLS] = { '0' };
+
+private:
+	ifstream levelFile;
+	int currentLevel = 1;
+};
